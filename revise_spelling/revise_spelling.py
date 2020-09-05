39dc5be7a86ecab78a179a2a04ef53ece1538914
sukhbinder singh <sukhbindersingh@sukh-3.local>
2020-09-05 07:50:25 +0530

    Added limit of 15 words at a time based on kids review

--- revise_spelling/revise_spelling.py
+++ revise_spelling/revise_spelling.py
@@ -27,6 +27,9 @@ CORRECT_RES = ["Thats Correct", "Correct", "Thats right. Way to go.", "Good Job.
 def get_words_to_reveiw(wordlist):
     now = datetime.now()
     selected_word = [word for word in wordlist if word.due_date < now]
+    # if more than 15 words, show only 10-15 words
+    if len(selected_word) > 15:
+        selected_word = selected_word[:np.random.randint(10,16)]
     if not selected_word:
         print("Nothing to review.")
         _say("Nothing to review.")
